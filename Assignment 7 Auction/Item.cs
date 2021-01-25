18ea861310e1d3a63aa2b6928680a373d28b87a0
Prem Worapon Sumsai <lulztit1929@gmail.com>
2021-01-25 20:36:27 +0700

    Add project files.

---
+++ Assignment 7 Auction/Item.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_7_Auction
+{
+    class Item
+    {
+        private string name;
+        private string description;
+        private double ReservePrice;
+        private double HighestBid;
+        private int ID;
+        public bool ChangeID = false;
+
+        public Item(string name, string description, double ReservePrice, double HighestBid, int ID)
+        {
+            this.name = name;
+            this.description = description;
+            this.ReservePrice = ReservePrice;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public string GetDescription()
+        {
+            return description;
+        }
+        public double GetReservePrice()
+        {
+            return ReservePrice;
+        }
+        public double GetHighestBid()
+        {
+            return HighestBid;
+        }
+        public int GetID()
+        {
+            return ID;
+        }
+        public bool setHighestBid(int newBid, int newID)
+        {
+            if (newBid > HighestBid)
+            {
+                HighestBid = newBid;
+                ID = newID;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void SetID(int newID)
+        {
+            if (ChangeID == true)
+            {
+                ID = newID;
+            }
+        }
+    }
+}
---
+++ Assignment 7 Auction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Assignment_7_Auction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item[] Items = new Item[5];
+            Items[0] = new Item("Diamond", "A rare diamond.", 100000, 90000, 123);
+            Items[1] = new Item("Gold", "A rare gold.", 2000, 1600, 321);
+            Items[2] = new Item("Silver", "A rare silver.", 500, 300, 125);
+
+            Items[1].setHighestBid(1000000, 199);
+
+            Items[2].setHighestBid(20000, 199);
+
+            for (int i = 0; i < Items.Length; i++)
+                if (Items[i] != null)
+            {
+                if (Items[i].GetHighestBid() > Items[i].GetReservePrice())
+                {
+                
+                    Console.WriteLine("ITEM NAME:" + Items[i].GetName());
+                    Console.WriteLine("SOLD FOR:" + Items[i].GetHighestBid());
+                    Console.WriteLine("BIDDER:" + Items[i].GetID());
+                }
+            }
+
+            
+        }
+        }
+    }
